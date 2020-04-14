8799119fedcfe0ed17612e0a0abec4d4392a0856
Minjae Kim <haedaal@gmail.com>
2020-04-14 15:59:54 +0900

    Machine Language Programming

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,50 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+    @8192
+    D=A
+    @limit
+    M=D
+(LOOP)
+    @offset
+    D=M
+    MD=D+1
+    @limit
+    D=M-D
+    @ROUND
+    D; JLE
+    @INPUT
+    0; JMP
+
+(ROUND)
+    @offset
+    M=0
+    @INPUT
+    0; JMP
+
+(INPUT)
+    @KBD
+    D=M
+    @PAINT_BLACK
+    D; JNE
+    @PAINT_WHITE
+    0; JMP
+
+
+(PAINT_BLACK)
+    @SCREEN
+    D=A
+    @offset
+    A=D+M
+    M=-1
+    @LOOP
+    0; JMP
+
+(PAINT_WHITE)
+    @SCREEN
+    D=A
+    @offset
+    A=D+M
+    M=0
+    @LOOP
+    0; JMP
